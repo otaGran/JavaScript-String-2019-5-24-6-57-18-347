edf194fa39cc78acd034371c3a5ef2896e36f623
otaGran <ujt2h8@gmail.com>
2019-06-23 23:30:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,25 @@
 // 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
 var name = 'hello';
 // TODO write your code here...
-
+name.toUpperCase();
 
 // 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
+function FirstToUpperCase(str) {
+  str = str.toLowerCase().split(' ')
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i].charAt(0)
+    str[i] = str[i].replace(char, function(s) {
+      return s.toUpperCase()
+    })
+  }
+  
+  console.log(str);
+}
 
 
 // 完成下面程序，将money变量中的金额提取出来，输出：20。
 var money = '￥20';
 //TODO write your code here...
+console.log(money.replace(/[^0-9]/ig,""));
